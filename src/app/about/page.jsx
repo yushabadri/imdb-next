bb12dd8967149db534f7335a2dbc88efcc4bf55f
farshad badri <yushabadri@gmail.com>
2024-02-24 14:23:24 +0300

    About Page Added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-500'> About </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor leo a diam sollicitudin tempor id. Aliquam ultrices sagittis orci a scelerisque purus. Congue eu consequat ac felis donec. Placerat in egestas erat imperdiet sed euismod nisi porta. Mi eget mauris pharetra et ultrices neque ornare aenean. Nisl vel pretium lectus quam id. Neque egestas congue quisque egestas. Elit sed vulputate mi sit amet mauris commodo quis. Enim sed faucibus turpis in eu mi bibendum neque.
+        </p> 
+        <p>
+            At volutpat diam ut venenatis tellus in metus. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Nullam non nisi est sit amet facilisis. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Ultricies mi eget mauris pharetra et ultrices. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Laoreet sit amet cursus sit amet dictum sit amet. Ac felis donec et odio pellentesque. Enim nec dui nunc mattis enim ut tellus elementum sagittis. Neque sodales ut etiam sit amet nisl purus in mollis. Sagittis eu volutpat odio facilisis mauris sit amet massa. Leo in vitae turpis massa sed. Sed risus pretium quam vulputate dignissim suspendisse. Ultrices dui sapien eget mi proin sed libero. Fames ac turpis egestas maecenas pharetra convallis posuere. Massa sed elementum tempus egestas sed sed.
+        </p> 
+        <p>
+            Ut ornare lectus sit amet. Viverra nam libero justo laoreet sit. Metus dictum at tempor commodo ullamcorper a lacus. Turpis massa tincidunt dui ut ornare lectus sit. Amet mauris commodo quis imperdiet massa tincidunt nunc. Mattis aliquam faucibus purus in massa tempor. Sagittis vitae et leo duis ut diam quam nulla. Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Non pulvinar neque laoreet suspendisse interdum. Elit sed vulputate mi sit. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Turpis massa sed elementum tempus egestas sed sed. Mi quis hendrerit dolor magna eget est. Ut pharetra sit amet aliquam. Aliquet eget sit amet tellus cras adipiscing enim.
+        </p>
+    </div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MenuItems from './MenuItems'
-import {AiFillHome} from 'react-icons/ai'
+import {AiFillHome, AiFillPhone, AiFillProject, AiFillRedEnvelope} from 'react-icons/ai'
 import Link from 'next/link'
 import DarkModeSwitch from './DarkModeSwitch'
 
@@ -9,7 +9,8 @@ export default function Header() {
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
         <div className="flex gap-4">
            <MenuItems title="home" address="/" Icon={AiFillHome}/>
-           <MenuItems title="about" address="/about" Icon={AiFillHome}/>
+           <MenuItems title="about" address="/about" Icon={AiFillProject}/>
+           <MenuItems title="contact" address="/contact" Icon={AiFillPhone}/>
         </div>
         <div className='flex items-center gap-4'>
             <DarkModeSwitch />
