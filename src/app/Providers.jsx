9104d9082505ee7bd4574448330e7ba1f673a4aa
farshad badri <yushabadri@gmail.com>
2024-02-24 14:06:26 +0300

    Dark Mode added

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -7,7 +7,7 @@ import { Children } from "react"
 export default function Providers({children}) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-        <div className="">
+        <div className="transition-color duration-300 dark: ">
             {children}
         </div>
     </ThemeProvider>
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -3,10 +3,12 @@
 import {MdLightMode , MdDarkMode} from 'react-icons/md'
 
 import { useTheme } from 'next-themes'
+import { useState } from 'react';
 
 export default function DarkModeSwitch() {
 
     const {theme,setTheme, systemTheme} = useTheme();
+    const {mounted, setMounted} = useState(false)
     const currentTheme = theme === 'system' ? systemTheme : theme
 
   return (
